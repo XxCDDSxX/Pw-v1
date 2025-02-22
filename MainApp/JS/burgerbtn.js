64e60e98bb39e06be10b252c71f82035cd168d34
Carlos Daniel De La Cruz Salvatierra <carlosd_salvatierra@outlook.com>
2025-02-22 15:01:13 -0600

    Se agregó un layout de workspace con un sidebar vacio funcional, se inicó la programación del menú de navegación principal

---
+++ MainApp/JS/burgerbtn.js
@@ -0,0 +1,8 @@
+var sidebar_toggle = window.screen.width <= 600 ? true : false;
+
+window.onload = showMenu();
+function showMenu() {
+   var menu = document.getElementById("main-sidebar");
+   menu.style.display = sidebar_toggle? 'none': 'inline';
+   sidebar_toggle = !sidebar_toggle;
+}
