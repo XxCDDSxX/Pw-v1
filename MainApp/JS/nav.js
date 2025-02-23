c315ffa0198b6601fea86ea1d77629960d130100
Carlos Daniel De La Cruz Salvatierra <carlosd_salvatierra@outlook.com>
2025-02-23 13:27:41 -0600

    Se cambió el nombre de la sección Dashboard por Home, ya que es más apropiado

--- MainApp/JS/nav.js
+++ MainApp/JS/nav.js
@@ -2,7 +2,7 @@ function setPage(option = 0){
     var workarea = document.getElementById("workpage");
     switch(option){
         case 0:
-            workarea.setAttribute("src","./Dashboard/index.html");
+            workarea.setAttribute("src","./Home/index.html");
             break;
         case 1:
             workarea.setAttribute("src","./Inscriptions/index.html");
